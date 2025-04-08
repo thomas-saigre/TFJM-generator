ace12049844a86bc269a25d8b26802ce5dd8cd3b
Thomas Saigre <thomas.saigre@math.unistra.fr>
2025-04-08 10:53:12 +0200

    fill pages with empty badges

--- scripts/generate_latex_badges.py
+++ scripts/generate_latex_badges.py
@@ -8,26 +8,45 @@ def formatName(nom, type_nom):
         return nom.title()
     return nom.upper()
 
+def fill(count, fd):
+    # Fill remaining slots on the last page with empty names
+    while count % 10 != 0:
+        latexiser("", "", "", fd)
+        count += 1
+    return count
+
 def latexiser(prenom, nom, role, fd):
-    fd.write("\\confpin{" + formatName(prenom, PRENOM) + " " + formatName(nom, NOM) + "}{" + role + "}\n")
+    if prenom == "" and nom == "" and role == "":
+        fd.write("\\confpin{}{}\n")
+    else:
+        fd.write("\\confpin{" + formatName(prenom, PRENOM) + " " + formatName(nom, NOM) + "}{" + role + "}\n")
 
 def run(participants, jury, orga, outdir="."):
     print("Generating badges...", end =" ")
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     fd = open(os.path.join(outdir, "participants.tex"), "w")
+    count = 0
     for _, row in participants.iterrows():
         latexiser(row['Prénom'], row['Nom'], f"Équipe {row['Trigramme']}", fd)
+        count += 1
+    fill(count, fd)
     fd.close()
 
     fd = open(os.path.join(outdir, "jury.tex"), "w")
+    count = 0
     for _, row in jury.iterrows():
         latexiser(row['Prénom'], row['Nom'], f"Membre du jury", fd)
+        count += 1
+    fill(count, fd)
     fd.close()
 
     fd = open(os.path.join(outdir, "orga.tex"), "w")
+    count = 0
     for _, row in orga.iterrows():
         latexiser(row['Prénom'], row['Nom'], f"Comité d'organisation/bénévole", fd)
+        count += 1
+    fill(count, fd)
     fd.close()
     print("Badges generated.")
 
